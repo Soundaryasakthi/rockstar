d9e565608f594dba3d0a77246d3f0b752d0cf764
Soundarya S <soundaryasakthi14@gmail.com>
2017-07-13 14:57:59 +0530

    Create starpattern.java

---
+++ starpattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class myCode
+{
+    public static void main (String[] args) 
+    {
+
+        Scanner scan=new Scanner(System.in);
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+                {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        
+    }
+}
