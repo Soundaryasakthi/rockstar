e66b3909d93151e3325844d64ea1a90063a7477b
Soundarya S <soundaryasakthi14@gmail.com>
2017-09-15 15:59:15 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class myCode
+{
+    public static void main (String[] args) 
+    {
+        int i;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(n+"*"+i+"="+n*i); 
+        }
+        
+    }
+}
+
+
